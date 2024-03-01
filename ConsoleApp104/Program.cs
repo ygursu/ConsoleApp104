84a5b2e1fbebf1578af643ac1054a932e3383c8a
ygursu <yasemingrs@gmail.com>
2024-03-01 21:53:32 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp104/Program.cs
@@ -0,0 +1,7 @@
+﻿int adet, fiyat;
+Console.Write("Almak istediğiniz ürün adedini giriniz:");
+adet = Convert.ToInt32(Console.ReadLine());
+Console.Write("Birim fiyatını giriniz:");
+fiyat = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Toplam ödemeniz: " + adet * fiyat + " TL'dir");
+Console.ReadKey();
